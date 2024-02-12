4132e606d0a93fb7fc3c438d2c175eba4396a427
Rodrigo Paris <rocostaparis@hotmail.com>
2024-02-12 19:12:09 -0300

    add new info for the portfolio

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,54 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  baseUrl: "",
+  name: "Rodrigo Paris",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `
+  I'm a São Paulo-based software developer with 2 years of experience in the Computer Software industry, specializing in both Front-end and Back-end Development.
+My proficiency includes HTML, CSS, SCSS, JavaScript, TypeScript, Tailwind CSS, React JS, Next JS for front-end, and Node JS, Express JS, Java, Spring Boot, NoSQL(MongoDB and Redis) and SQL(Postgres) for back-end development.   
+   Having a proven track record of successfully delivering web projects from inception, 
+   I am committed to ongoing learning and the exploration of emerging technologies.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Freelancer Developer",
+      location: "São Paulo, Brasil",
+      startDate: "Aug 2022",
       endDate: "Present",
       description: [
         "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "- Fixing UI issues and integrating with backend APIs.",
+        "- Implementing code in compliance with relevant design and architectural guidelines.",
+        "- Documenting, testing, deploying, maintaining, and supporting platforms, applications, databases, and solutions.",
+        "- Enhancing code through improvements and refactoring.",
+        "- Upholding best practices and rules."
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "Universidade Anhembi Morumbi",
+      location: "São Paulo, São Paulo, ",
       startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
       endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
       description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/RoParis9",
+    email: "mailto:rocostaparis@hotmail.com",
+    linkedin: "https://www.linkedin.com/in/rodrigo-costa-paris/",
   },
 
   projects: [
-    {
-      title: "Astro Portfolio",
-      isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
-    },
+    // {
+    //   title: "Portfolio",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    //   liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    // },
   ],
 };
